6e413ff65a522a8504971b12cdb4ccac16f13d5a
ace411 <lochbm@live.com>
2021-05-24 13:52:04 +0300

    changed coverage file path

--- .atoum.php
+++ .atoum.php
@@ -7,7 +7,7 @@ use atoum\atoum\writers\std;
 $script->addDefaultReport();
 
 $clover = new \atoum\atoum\reports\sonar\clover();
-$writer = new \atoum\atoum\writers\file('./reports/atoum.coverage.xml');
+$writer = new \atoum\atoum\writers\file('coverage.xml');
 $clover->addWriter($writer);
 $runner->addReport($clover);
 $runner->addTestsFromDirectory('./tests');
